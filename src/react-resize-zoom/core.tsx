d42e31bc38dc5802ff1b7ac993db9d2f8ba474fc
mezhanglei <496623925@qq.com>
2021-11-14 22:04:22 +0800

    fix: update resize zoom

--- src/react-resize-zoom/core.tsx
+++ src/react-resize-zoom/core.tsx
@@ -38,16 +38,16 @@ class DragResize extends React.Component<DragResizeProps, DragResizeState> {
     }
 
     componentDidUpdate(prevProps: DragResizeProps, prevState: DragResizeState) {
-        const widthChanged = this.props.width !== prevProps.width;
-        const heightChanged = this.props.height !== prevProps.height;
+        const widthChanged = this.props.width !== undefined && (this.props.width !== prevProps.width || this.props.width !== prevState.eventData?.width);
+        const heightChanged = this.props.height !== undefined && (this.props.height !== prevProps.height || this.props.height !== prevState.eventData?.height);
         if (widthChanged || heightChanged) {
             this.updateState()
         }
     }
 
     static getDerivedStateFromProps(nextProps: DragResizeProps, prevState: DragResizeState) {
-        const widthChanged = nextProps.width !== prevState.prevWidth;
-        const heightChanged = nextProps.height !== prevState.prevHeight;
+        const widthChanged = nextProps.width !== undefined && (nextProps.width !== prevState.prevWidth || nextProps.width !== prevState.eventData?.width);
+        const heightChanged = nextProps.height !== undefined && (nextProps.height !== prevState.prevHeight || nextProps.height !== prevState.eventData?.height);
         if (widthChanged) {
             return {
                 ...prevState,
@@ -86,7 +86,7 @@ class DragResize extends React.Component<DragResizeProps, DragResizeState> {
     }
 
     findDOMNode() {
-        return this.props?.forwardedRef?.current || ReactDOM.findDOMNode(this);
+        return this.props?.forwardedRef.current || ReactDOM.findDOMNode(this);
     }
 
     // 顶层document对象（有的环境可能删除了document顶层环境）
